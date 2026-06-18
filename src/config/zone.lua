53c53887b04a775339880ad1bcafa0709def2c28
0xS4cha | sservant <servantonsacha@gmail.com>
2026-06-18 23:38:01 +0200

    fix/colors of zones

--- src/config/zone.lua
+++ src/config/zone.lua
@@ -2,9 +2,9 @@ local ZONES = {
     Heightmap = {
         { name = "Water",   min = 0,   max = 50,  r = 61,   g = 123,  b = 171 },
         { name = "Sand",            min = 101, max = 120, r = 210, g = 190, b = 130 },
-        { name = "Plaine",          min = 121, max = 170, r = 60,  g = 140, b = 40  },
-        { name = "Forest",          min = 171, max = 210, r = 30,  g = 90,  b = 20  },
-        { name = "Mountain",        min = 211, max = 240, r = 120, g = 100, b = 80  },
+        { name = "Plaine",          min = 121, max = 170, r = 190, g = 212, b = 138  },
+        { name = "Forest",          min = 171, max = 210, r = 190, g = 212, b = 138  },
+        { name = "Mountain",        min = 211, max = 240, r = 220, g = 203, b = 158  },
         { name = "Snow",            min = 241, max = 255, r = 240, g = 245, b = 255 }
     }
 }

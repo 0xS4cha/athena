e453b3eaaa801ed19c2ed3c1d83469ebc77a533a
0xS4cha | sservant <servantonsacha@gmail.com>
2026-07-21 19:59:49 +0200

    feat/start to implement city expansion

--- src/modules/building/city.lua
+++ src/modules/building/city.lua
@@ -5,7 +5,12 @@ return {
     scale = 0.95,
     color = { 0.95, 0.85, 0.25 },
     outline = { 0.25, 0.2, 0.05, 0.75 },
-    think = function(building, dt)
-        building.state.influence = math.min(1, (building.state.influence or 0) + dt * 0.03)
-    end
+    influenceThreshold = 100,
+	think = function(building, dt)
+		building.state.influence = math.min(1, (building.state.influence or 0) + dt * 0.03)
+        local threshold = building.definition.influenceThreshold or 100
+		if building.state.influence + 1 % threshold >= threshold then
+			do end
+		end
+	end
 }
